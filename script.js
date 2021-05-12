bc2ecfb8f0f65c02dc991dc773c40a7bea3eb09c
r4korompis <r4korompis@gmail.com>
2021-05-12 12:16:01 +0800

    proses pembuatan landing page (on-progres)

--- script.js
+++ script.js
@@ -2,21 +2,9 @@
 const humburgerMenuInput = document.querySelector(".humburger-menu input");
 const nav = document.querySelector(".navigation-link");
 const link = document.querySelectorAll(".navigation-link li");
-const main = document.querySelector("#main");
-const headMain = document.querySelector("#main-header");
 
 humburgerMenuInput.addEventListener("click", function () {
   nav.classList.toggle("slide");
-});
-
-main.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-  humburgerMenuInput.checked = false;
-});
-
-headMain.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-  humburgerMenuInput.checked = false;
 });
 
 link.forEach(function (e) {
